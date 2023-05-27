b91d6c9970d89606600565bfd4ce6679bafaa97b
rgbuscar <132348224+rgbuscar@users.noreply.github.com>
2023-05-27 23:55:07 +0800

    Add files via upload

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -65,6 +65,6 @@ class SubjectsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def subject_params
-      params.require(:subject).permit(:name, :section, :number_of_units, :teacher_id, :avatar)
+      params.require(:subject).permit(:name, :section, :number_of_units, :teacher_id)
     end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,5 +2,24 @@ class Student < ApplicationRecord
   has_many :teachers
   has_many :courses
   has_many :subjects, through: :courses
-  
+  has_one_attached :avatar
+  validate :acceptable_image
+
+  def acceptable_image
+       return unless avatar.attached?
+
+       unless avatar.blob.byte_size <= 1.megabyte
+         errors.add(:avatar, "is too big")
+       end
+
+       acceptable_types = ["image/jpeg", "image/png"]
+       unless acceptable_types.include?(avatar.content_type)
+         errors.add(:avatar, "must be a JPEG or PNG")
+       end
+     end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
 end
