8d64be060580517a36d000dc01626305af25bcf8
rgbuscar <132348224+rgbuscar@users.noreply.github.com>
2023-05-28 01:01:02 +0800

    Add files via upload

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -65,6 +65,6 @@ class TeachersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def teacher_params
-      params.require(:teacher).permit(:name, :department, :per_unit_rate)
+      params.require(:teacher).permit(:name, :department, :per_unit_rate, :avatar)
     end
 end
